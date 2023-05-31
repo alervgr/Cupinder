a8acd3756f4d69f1ee756305128d1435c23ffade
lgomvia <lgomvia@alu.upo.es>
2023-05-31 17:51:35 +0200

    perfil perfecto

--- Cupinder/src/java/acciones/perfilAcciones.java
+++ Cupinder/src/java/acciones/perfilAcciones.java
@@ -13,19 +13,32 @@ import java.util.Map;
 import modelos.Facultades;
 import modelos.TarjetasDeCredito;
 import modelos.Usuarios;
+import org.apache.struts2.interceptor.validation.SkipValidation;
 import persistencia.DAO_facultades;
 import persistencia.DAO_tarjeta;
 import persistencia.DAO_usuario;
 
 public class perfilAcciones extends ActionSupport {
-    
+
     private final DAO_facultades dao_f;
     private final DAO_tarjeta dao_t;
     private final DAO_usuario dao_u;
+
+    private String passwordConfirm;
+    private String password;
+    private String email;
+    private String dni;
+    private String orientacion;
+    private String facultad;
+    private String ocupacion;
+    private String idioma;
+    private String bio;
+    private String edad;
+
     private List<String> facultades;
     private List<TarjetasDeCredito> tarjetasUsuario;
     private String fac;
-    
+
     public perfilAcciones() {
         dao_f = new DAO_facultades();
         dao_t = new DAO_tarjeta();
@@ -33,21 +46,78 @@ public class perfilAcciones extends ActionSupport {
         facultades = new ArrayList();
         tarjetasUsuario = new ArrayList();
     }
-    
-    public String execute() throws Exception {
-        
+
+    public void validate() {
+
+        if (0 == this.getEmail().length() || null == this.getEmail()) {
+            addFieldError("correo", getText("correo.relleno"));
+        }
+        if (0 == this.getPassword().length() || null == this.getPassword()) {
+            addFieldError("password", getText("contrasenia.rellena"));
+        } else if (0 == this.getPasswordConfirm().length() || null == this.getPasswordConfirm()) {
+            addFieldError("passwordConfirm", getText("contrasenia.rellena"));
+        } else if (!this.getPassword().equals(this.getPasswordConfirm())) {
+            addFieldError("passwordConfirm", getText("contrasenia.diferente"));
+        }
+        if (0 == this.getOrientacion().length() || null == this.getOrientacion()) {
+            addFieldError("orientacion", getText("orientacion.rellena"));
+        }
+        if (0 == this.getFacultad().length() || null == this.getFacultad()) {
+            addFieldError("facultad", getText("facultad.rellena"));
+        }
+        if (0 == this.getOcupacion().length() || null == this.getOcupacion()) {
+            addFieldError("ocupacion", getText("ocupacion.rellena"));
+        }
+        if (0 == this.getIdioma().length() || null == this.getIdioma()) {
+            addFieldError("idioma", getText("idioma.relleno"));
+        }
+        if (0 == this.getEdad().length() || null == this.getEdad()) {
+            addFieldError("edad", getText("edad.rellena"));
+        }
+
         Map session = (Map) ActionContext.getContext().get("session");
         Usuarios user = (Usuarios) session.get("user");
-        System.out.println(user.getNombre());
-        System.out.println(user.getId());
-        
-        
-        
         this.setFac(dao_f.getFacultadId(user.getId()));
-        
+
         this.setFacultades(this.dao_f.listadoFacultades());
         this.setTarjetasUsuario(this.dao_t.obtenerTarjetasDeCredito(user.getId()));
-        
+
+    }
+
+    @SkipValidation
+    public String execute() throws Exception {
+
+        Map session = (Map) ActionContext.getContext().get("session");
+        Usuarios user = (Usuarios) session.get("user");
+
+        this.setFac(dao_f.getFacultadId(user.getId()));
+
+        this.setFacultades(this.dao_f.listadoFacultades());
+        this.setTarjetasUsuario(this.dao_t.obtenerTarjetasDeCredito(user.getId()));
+
+        return SUCCESS;
+    }
+
+    public String actualizar() {
+
+        Map session = (Map) ActionContext.getContext().get("session");
+        Usuarios user = (Usuarios) session.get("user");
+        Facultades facultad = this.dao_f.getFacultadNombre(this.getFacultad());
+
+        user.setCorreo(this.getEmail());
+        user.setDni(this.getDni());
+        user.setEdad(Integer.parseInt(this.getEdad()));
+        user.setFacultades(facultad);
+        user.setIdioma(this.getIdioma());
+        user.setOcupacion(this.getOcupacion());
+        user.setOrientacion(this.getOrientacion());
+        user.setBiografia(this.getBio());
+        user.setPassword(this.getPassword());
+
+        this.dao_u.actualizarUsuario(user);
+
+        session.replace("user", user);
+
         return SUCCESS;
     }
 
@@ -74,5 +144,85 @@ public class perfilAcciones extends ActionSupport {
     public void setFac(String fac) {
         this.fac = fac;
     }
-    
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getOrientacion() {
+        return orientacion;
+    }
+
+    public void setOrientacion(String orientacion) {
+        this.orientacion = orientacion;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
 }
--- Cupinder/web/perfil/perfil.jsp
+++ Cupinder/web/perfil/perfil.jsp
@@ -37,28 +37,36 @@
                                     <div class="col-md-6"><label class="labels">Nombre</label><s:textfield cssClass="form-control" value="%{#session.user.nombre}" disabled="true"></s:textfield></div>
                                     <div class="col-md-6"><label class="labels">Apellidos</label><s:textfield cssClass="form-control" value="%{#session.user.apellidos}" disabled="true"></s:textfield></div>
                                     <div class="col-md-9"><label class="labels">Nombre de usuario</label><s:textfield cssClass="form-control" value="%{#session.user.usuario}" disabled="true"></s:textfield></div>
-                                    <div class="col-md-3"><label class="labels">Edad</label><s:textfield cssClass="form-control" value="%{#session.user.edad}"></s:textfield></div>
-                                    <div class="col-md-12"><label class="labels">Email</label><s:textfield cssClass="form-control" value="%{#session.user.correo}"></s:textfield></div>
-                                    <div class="col-md-6"><label class="labels">Contraseña</label><s:textfield cssClass="form-control" value=""></s:textfield></div>
-                                    <div class="col-md-6"><label class="labels">Confirmar Contraseña</label><s:textfield cssClass="form-control" value=""></s:textfield></div>
+                                    <div class="col-md-3"><label class="labels">Edad</label><s:textfield name="edad" cssClass="form-control" value="%{#session.user.edad}"></s:textfield></div>
+                                    <div class="col-md-12"><label class="labels">Email</label><s:textfield name="email" cssClass="form-control" value="%{#session.user.correo}"></s:textfield></div>
 
                                     </div>
                                     <div class="row mt-3">
-                                        <div class="col-md-12"><label class="labels">Idioma</label><s:textfield cssClass="form-control" value="%{#session.user.idioma}"></s:textfield></div>
+                                        <div class="col-md-12"><label class="labels">Idioma</label><s:textfield name="idioma" cssClass="form-control" value="%{#session.user.idioma}"></s:textfield></div>
                                     <div class="col-md-12"><label class="labels">Facultad</label><s:select name="facultad" label="Facultad" cssClass="form-control" placeholder="Facultad" list="facultades" value="fac"/></div>
-                                    <div class="col-md-12"><label class="labels">Ocupación</label><s:textfield cssClass="form-control" value="%{#session.user.ocupacion}"></s:textfield></div>
-                                    <div class="col-md-12"><label class="labels">DNI</label><s:textfield cssClass="form-control" value="%{#session.user.DNI}"></s:textfield></div>
-                                    <div class="col-md-12"><label class="labels">Biografía</label><s:textarea cssClass="form-control" value="%{#session.user.biografia}"></s:textarea></div>
+                                    <div class="col-md-12"><label class="labels">Ocupación</label><s:textfield name="ocupacion" cssClass="form-control" value="%{#session.user.ocupacion}"></s:textfield></div>
+                                    <div class="col-md-12"><label class="labels">DNI</label><s:if test="%{#session.user.dni == null}"><s:textfield name="dni" cssClass="form-control"></s:textfield></s:if><s:else><s:textfield name="dni" cssClass="form-control" value="%{#session.user.dni}" disabled="true"></s:textfield></s:else></div>
+                                    <div class="col-md-12"><label class="labels">Biografía</label><s:textarea name="bio" cssClass="form-control" value="%{#session.user.biografia}"></s:textarea></div>
 
                                     </div>
                                     <div class="row mt-3">
                                         <div class="col-md-6"><label class="labels">Género</label><s:textfield cssClass="form-control" value="%{#session.user.genero}" disabled="true"></s:textfield></div>
                                     <div class="col-md-6"><label class="labels">Orientación</label><s:select name="orientacion" label="Orientacion" cssClass="form-control" placeholder="Orientacion" value="%{#session.user.orientacion}" list="{'Heterosexual','Homosexual','Bisexual'}"/></div>
                                 </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-6"><label class="labels">Contraseña</label><s:password name="password" cssClass="form-control" value=""></s:password></div>
+                                    <div class="col-md-6"><label class="labels">Confirmar Contraseña</label><s:password name="passwordConfirm" cssClass="form-control" value=""></s:password></div>
+                                    <s:fielderror cssClass="pt-3"></s:fielderror>
+                                    </div>
+
+                                </div>
+
                             </div>
+                            <div class="mb-5 text-center"><button class="btn btnRegistro" type="submit">Guardar Perfil</button></div>
                         </div>
-                        <div class="mb-5 text-center"><button class="btn btnRegistro" type="submit">Guardar Perfil</button></div>
-                    </div>
+
+
+
                 </s:form>
                 <div>
                     <hr class="m-2">
